5d2b64f600ab9e0c36053b901e665e0fae0e666f
Rafael Luiz <faelluizrosa91@gmail.com>
2026-04-27 20:55:57 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const CHECKOUT_URL = "https://go.sharkpayments.com.br/wmfth8t5fr";
+const CHECKOUT_URL = "https://seguropagamentos.com.br/of3";
 
 const Index = () => {
   const [popup1Visible] = useState(true); // visible immediately
