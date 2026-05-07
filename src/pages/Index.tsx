88278d519a1a4a11d1258318d006f9250c9c6e49
Rafael Luiz <faelluizrosa91@gmail.com>
2026-05-07 00:24:27 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const CHECKOUT_URL = "https://compraseguraonline.org.ua/c/38c4605730";
+const CHECKOUT_URL = "https://compraonlinesegurada.org.ua/c/38c4605730";
 
 const Index = () => {
   const [popup1Visible] = useState(true); // visible immediately
